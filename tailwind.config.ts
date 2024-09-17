c2b0d298851f8b917b2133ea8c1b4e464398c6e3
Zülal <zllnbn1997@gmail.com>
2024-09-17 11:57:09 +0300

    install fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      fontFamily: {
+        acme: ["var(--font-acme)"],
+        galano: ["var(--font-galano)"]
+      }
     },
   },
   plugins: [],
