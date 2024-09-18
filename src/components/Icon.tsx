875c7662fadb4b9ed6d5634da3d809b906972c99
Zülal <zllnbn1997@gmail.com>
2024-09-18 11:31:01 +0300

    convert js files to ts

---
+++ src/components/Icon.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Add, Calendar, Confirm, Location, Menu, Search } from "@/assets/icons"; // Import all icons from the folder
+
+type IconName = "add" | "calendar" | "confirm" | "location" | "menu" | "search";
+
+// Use React.FunctionComponent<React.SVGProps<SVGSVGElement>> to type the components
+const iconMap: Record<
+  IconName,
+  React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+> = {
+  add: Add,
+  calendar: Calendar,
+  confirm: Confirm,
+  location: Location,
+  menu: Menu,
+  search: Search,
+};
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  name: IconName;
+  size?: number;
+}
+
+export const Icon: React.FC<IconProps> = ({ name, size = 24, ...others }) => {
+  const IconComponent = iconMap[name];
+
+  if (!IconComponent) return null;
+
+  return <IconComponent {...others} width={size} height={size} />;
+};
