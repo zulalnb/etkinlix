2400f4298bff0b180745bebcde0e0ca7e38cc7db
Zülal Nebin <zllnbn1997@gmail.com>
2025-09-16 15:50:44 +0300

    ux fix

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -10,19 +10,25 @@ export const EventList: FC = () => {
 
 	return (
 		<div className="space-y-4">
-			{state.filteredEvents.map((event: Event) => (
-				<EventCard
-					key={event.id}
-					id={event.id}
-					title={event.title}
-					description={event.description}
-					type={event.type}
-					date={event.date}
-					location={event.location}
-					image={event.image}
-					isAddedToCalendar={event.isAddedToCalendar}
-				/>
-			))}
+			{state.filteredEvents.length < 1 && (
+				<div className="flex items-center justify-center">
+					<p className="py-5 text-center text-3xl font-medium">Aradığınız Etkinlik Bulunamadı...</p>
+				</div>
+			)}
+			{state.filteredEvents &&
+				state.filteredEvents.map((event: Event) => (
+					<EventCard
+						key={event.id}
+						id={event.id}
+						title={event.title}
+						description={event.description}
+						type={event.type}
+						date={event.date}
+						location={event.location}
+						image={event.image}
+						isAddedToCalendar={event.isAddedToCalendar}
+					/>
+				))}
 		</div>
 	);
 };
