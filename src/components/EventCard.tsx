ba307573ba3b053607fcb1584757704807bf49de
Zülal Nebin <zllnbn1997@gmail.com>
2025-09-17 10:32:02 +0300

    bug fix

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -11,8 +11,7 @@ import { useEvent } from "@/context/EventProvider";
 import { useMobileView } from "@/hooks/useMobileMenu";
 import { clipText } from "@/lib/utils";
 import { Event } from "@/types/Event";
-import moment from "moment";
-import "moment/locale/tr"; // import Turkish locale
+import moment from "@/lib/moment-config";
 
 export const EventCard: FC<Event> = ({
 	id,
@@ -27,45 +26,43 @@ export const EventCard: FC<Event> = ({
 	const isMobile = useMobileView();
 	const { dispatch } = useEvent();
 
-	moment().locale("tr");
-
 	// Add event to calendar
 	const toggleCalendar = (id: number) => {
 		dispatch({ type: "TOGGLE_CALENDAR", id });
 	};
 
 	return (
-		<div className="mb-4 h-112 w-full border border-light-gray md:h-52">
+		<div className="border-light-gray mb-4 h-112 w-full border md:h-52">
 			<div className="h-full w-full md:flex">
 				<div className="relative h-60 w-full md:h-full md:w-1/3">
 					<div className="absolute top-0 z-10 h-52 w-full bg-black md:left-0 md:h-full md:w-1/2">
-						<div className="mt-2 flex justify-center space-x-1 md:ml-1 md:mt-auto md:h-full md:w-1/2 md:flex-col md:space-x-0">
+						<div className="mt-2 flex justify-center space-x-1 md:mt-auto md:ml-1 md:h-full md:w-1/2 md:flex-col md:space-x-0">
 							{moment(date)
 								.format("D MMMM dddd HH:mm")
 								.split(" ")
 								.map((d) => (
-									<span key={d} className="text-center font-acme text-white md:inline-block md:text-pink">
+									<span key={d} className="font-acme md:text-pink text-center text-white md:inline-block">
 										{d}
 									</span>
 								))}
 						</div>
 					</div>
-					<div className="absolute bottom-0 z-20 h-48 w-full md:bottom-auto md:right-0 md:h-full md:w-3/4">
+					<div className="absolute bottom-0 z-20 h-48 w-full md:right-0 md:bottom-auto md:h-full md:w-3/4">
 						<div className="relative flex h-full w-full items-center justify-center">
 							<div className="relative aspect-square h-full w-4/5 overflow-hidden md:h-3/4 md:w-full">
 								<Image src={image} alt={title} fill className="absolute inset-0 object-cover object-center" />
 							</div>
-							<EventType type={type} className="absolute -left-6 -top-10 z-30 sm:-left-3 md:top-10" />
+							<EventType type={type} className="absolute -top-10 -left-6 z-30 sm:-left-3 md:top-10" />
 						</div>
 					</div>
 				</div>
-				<div className="mb-2.5 mt-8 flex flex-col justify-center overflow-auto px-8 md:mb-auto md:ml-6 md:mr-10 md:mt-auto md:w-1/2 md:px-4 md:py-2">
-					<p className="mb-2.5 cursor-pointer font-acme text-lg transition-colors hover:text-orange">
+				<div className="mt-8 mb-2.5 flex flex-col justify-center overflow-auto px-8 md:mt-auto md:mr-10 md:mb-auto md:ml-6 md:w-1/2 md:px-4 md:py-2">
+					<p className="font-acme hover:text-orange mb-2.5 cursor-pointer text-lg transition-colors">
 						{title}
 					</p>
 					<div className="mb-2.5 flex items-center space-x-1">
 						<Icon name="location" size={14} className="text-medium-gray" />
-						<span className="text-sm text-medium-gray">{location}</span>
+						<span className="text-medium-gray text-sm">{location}</span>
 					</div>
 					<p>
 						{clipText(description, isMobile ? 4 : 24)}
---
+++ src/lib/moment-config.ts
@@ -0,0 +1,6 @@
+// moment-config.ts
+import moment from "moment";
+import "moment/locale/tr";
+
+moment.locale("tr");
+export default moment;
