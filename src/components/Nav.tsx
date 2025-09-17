19e9867c39370115cf1c87a811a45f1f0eae18d4
Zülal Nebin <zllnbn1997@gmail.com>
2025-09-17 10:15:18 +0300

    remove scrollbar-hide class

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,10 +4,10 @@ import { SearchInput } from "./SearchInput";
 export const Nav = () => {
 	return (
 		<nav className="relative mt-10 flex h-12 w-full lg:h-20">
-			<ul className="scrollbar-hide mx-auto flex gap-5 overflow-x-scroll whitespace-nowrap px-4 md:gap-8 md:px-0">
+			<ul className="mx-auto flex gap-5 overflow-x-scroll px-4 whitespace-nowrap md:gap-8 md:px-0">
 				<NavLinks />
 			</ul>
-			<div className="absolute right-0 top-1/2 hidden -translate-y-1/2 pr-4 lg:block">
+			<div className="absolute top-1/2 right-0 hidden -translate-y-1/2 pr-4 lg:block">
 				<SearchInput />
 			</div>
 		</nav>
