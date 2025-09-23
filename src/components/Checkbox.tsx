adda332cac66deaf444488d04384392c90750e05
Zülal Nebin <zllnbn1997@gmail.com>
2025-09-23 23:36:03 +0300

    fix checkbox styling

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,13 +2,14 @@ interface CheckboxProps<TName extends string> {
 	name: TName;
 	label: string;
 }
+
 export const Checkbox = <TName extends string>({ name, label }: CheckboxProps<TName>) => {
 	return (
 		<label className="inline-flex items-center gap-x-2 py-3">
 			<input
 				name={name}
 				type="checkbox"
-				className="border-light-gray text-pink shadow-xs focus:border-dark-pink focus:ring-3 focus:ring-dark-pink focus:ring-opacity-50 focus:ring-offset-0"
+				className="border-light-gray checked:border-dark-pink text-pink focus:border-dark-pink focus:ring-dark-pink focus:ring-opacity-50 size-5 focus:ring-1 focus:ring-offset-0"
 			/>
 			<span>{label}</span>
 		</label>
