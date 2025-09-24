86020054cb061b36bd82f9ed58fc3e4445ea35f1
Zülal Nebin <zllnbn1997@gmail.com>
2025-09-24 12:43:19 +0300

    add custom SVG check icon for checked state in checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,12 +5,16 @@ interface CheckboxProps<TName extends string> {
 
 export const Checkbox = <TName extends string>({ name, label }: CheckboxProps<TName>) => {
 	return (
-		<label className="inline-flex items-center gap-x-2 py-3">
+		<label className="relative block cursor-pointer py-3 pl-7.5 select-none">
 			<input
+				id={name}
 				name={name}
 				type="checkbox"
-				className="border-light-gray checked:border-dark-pink text-pink focus:border-dark-pink focus:ring-dark-pink focus:ring-opacity-50 size-5 focus:ring-1 focus:ring-offset-0"
+				className="peer absolute size-0 cursor-pointer opacity-0"
 			/>
+
+			{/* Custom visual box */}
+			<span className="border-light-gray peer-checked:bg-pink peer-checked:border-dark-pink absolute top-1/2 left-0 size-5 -translate-y-1/2 border after:absolute after:top-1/2 after:left-1/2 after:hidden after:h-2 after:w-2.75 after:-translate-1/2 after:bg-white after:[mask-image:url('/assets/icons/check.svg')] after:[mask-size:contain] after:[mask-repeat:no-repeat] after:content-[''] peer-checked:after:block"></span>
 			<span>{label}</span>
 		</label>
 	);
