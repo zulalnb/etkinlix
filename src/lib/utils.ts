9dcdc7f78b7d1d088bf61fa8975bd77b16788e60
Zülal Nebin <zllnbn1997@gmail.com>
2025-09-26 20:17:02 +0300

    refactor helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,7 @@
 export const clipText = (str: string, limit = 145) => {
 	if (str.length <= limit) return str;
 
-	return str.split(" ").reduce((acc, word) => {
-		if ((acc + " " + word).trim().length > limit) return acc;
-		return (acc + " " + word).trim();
-	}, "");
+	return str
+		.split(" ")
+		.reduce((acc, word) => ((acc + " " + word).trim().length > limit ? acc : (acc + " " + word).trim()), "");
 };
